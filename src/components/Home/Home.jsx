a25d375e25d8824b018e610a80281b8dffc1c411
antar-cd <sagar.chandradas1994@gmail.com>
2023-04-09 19:44:34 +0600

    added order summary & toast

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,49 @@
-import React from 'react';
-import {useLoaderData} from 'react-router-dom'
+import React, { useState } from 'react';
+import { useLoaderData } from 'react-router-dom'
+import Tshirt from '../Tshirt/Tshirt';
+import Cart from '../Cart/Cart';
+import './Home.css'
+import toast from 'react-hot-toast';
+
 const Home = () => {
+    const [cart, setCart] = useState([])
     const tshirts = useLoaderData();
+
+    const handleAddToCart = tshirt => {
+        const exists = cart.find(ts => ts._id === tshirt._id);
+        if (exists) {
+            toast('you have already added');
+        }
+        else {
+            const newCart = [...cart, tshirt];
+            setCart(newCart)
+        }
+    }
+    const handleRemoveFromCart = id => {
+
+        const remaimimg = cart.filter(ts => ts._id !== id);
+        setCart(remaimimg)
+    }
+
     return (
-        <div>
-            <h2>This is Home : {tshirts.length}</h2>
+        <div className='home-container'>
+            <div className='tshirt-container'>
+                {
+                    tshirts.map(tshirt => <Tshirt
+
+                        key={tshirt._id}
+                        tshirt={tshirt}
+                        handleAddToCart={handleAddToCart}
+                    ></Tshirt>)
+                }
+            </div>
+            <div className='cart-container'>
+                <Cart
+                    cart={cart}
+                    handleRemoveFromCart={handleRemoveFromCart}
+                ></Cart>
+            </div>
+
         </div>
     );
 };
